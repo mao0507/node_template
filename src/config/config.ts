5ef4c7c64523ea89d9c9cf33ece6e935952f2cb0
mao <37288931+mao0507@users.noreply.github.com>
2023-03-25 17:18:40 +0800

    feat:config增加註解

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,12 @@
 /* config.js */
 // require and configure dotenv, will load vars in .env in PROCESS.ENV
 
-
+/**
+ * 通用設定位置
+ * env 環境名稱
+ * port 要開放的port
+ * version 版本號
+ */
 const config = {
   version: '1.0.0',
   env: 'development',

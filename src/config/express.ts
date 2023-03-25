e558d127e85f71bdbf9d33d5d519a5e409cdf855
mao <37288931+mao0507@users.noreply.github.com>
2023-03-25 17:14:38 +0800

    fix:移除多餘部分

--- src/config/express.ts
+++ src/config/express.ts
@@ -12,21 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
-
 /* GET home page. */
 app.get('/', (req, res) => {
   res.send(`server started on  port http://127.0.0.1:${config.port} (${config.env})`);
 });
-
-/**
- * 
- * index 進入router
- * 
- */
-import index from '../server/router/index';
-
-app.use('/api', index);
-
 
 
 export default app;

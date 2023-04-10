b3f36fff343cd4429497d0885de6491af5756803
Mao <37288931+mao0507@users.noreply.github.com>
2023-04-11 01:36:24 +0800

    feat:設定swagger tag

--- src/server/router/test.ts
+++ src/server/router/test.ts
@@ -4,11 +4,23 @@ const router = express.Router();
 
 /* GET localhost:[port]/api page. */
 router.get('/', (req, res) => {
+  /* 
+    #swagger.tags = ['test']
+    #swagger.s ecurity = [{
+      "b earerAuth":[]
+    }]
+  } */
+  res.status(200)
   res.send(`此路徑是: localhost:${config.port}/api/test`);
 });
 
 router.get('/list',(req,res) => {
+    /* 
+    #swagger.tags = ['test']
+  } */
+  res.status(200)
   res.send('list')
 })
 
-export default router;
+
+module.exports = router ;
